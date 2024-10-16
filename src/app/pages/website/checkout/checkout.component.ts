1d006acf7f372d77f50667fb8595c522b1e8766a
RanaGamal11 <shawkat.gamal11@gmail.com>
2024-10-16 09:05:44 +0300

    checkout

--- src/app/pages/website/checkout/checkout.component.ts
+++ src/app/pages/website/checkout/checkout.component.ts
@@ -1,12 +1,104 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-checkout',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule, RouterLink],
   templateUrl: './checkout.component.html',
-  styleUrl: './checkout.component.css'
+  styleUrls: ['./checkout.component.css']
 })
-export class CheckoutComponent {
+export class CheckoutComponent implements OnInit {
 
+  cartList: any[] = [];
+  shippingFee: number = 50;
+  totalPrice: number = 0;
+  selectedPaymentMethod: string = ''; 
+  checkoutForm: FormGroup;
+  paymentMethodError: boolean = false; 
+  isOnlinePaymentSelected: boolean = false;
+  constructor(private fb: FormBuilder, private router: Router) {
+    this.checkoutForm = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      phone: ['', [Validators.required, Validators.pattern('^[0-9]*$')]],
+      email: ['', [Validators.required, Validators.email]],
+      country: ['Egypt', Validators.required],
+      streetNumber: ['', Validators.required],
+      state: ['', Validators.required],
+      city: ['', Validators.required],
+      cardNumber: [''],
+      cardHolderName: [''],
+      expiryMonth: [''],
+      expiryYear: [''],
+      cvv: ['']
+    });
+  }
+
+  ngOnInit(): void {
+    this.getCartDataFromLocalStorage();
+    this.calculateTotalPrice();
+  }
+
+  getCartDataFromLocalStorage() {
+    const cartData = localStorage.getItem('customer_cart');
+    if (cartData) {
+      this.cartList = JSON.parse(cartData);
+    }
+  }
+
+  calculateTotalPrice() {
+    const subtotal = this.cartList.reduce((total, item) => total + (item.productPrice * item.quantity), 0);
+    this.totalPrice = subtotal + this.shippingFee;
+  }
+
+ 
+  selectPaymentMethod(method: string) {
+    this.selectedPaymentMethod = method;
+    this.paymentMethodError = false;
+
+    if (method === 'Online') {
+      this.isOnlinePaymentSelected = true;
+
+      this.checkoutForm.get('cardNumber')?.setValidators([Validators.required]);
+      this.checkoutForm.get('cardHolderName')?.setValidators([Validators.required]);
+      this.checkoutForm.get('expiryMonth')?.setValidators([Validators.required]);
+      this.checkoutForm.get('expiryYear')?.setValidators([Validators.required]);
+      this.checkoutForm.get('cvv')?.setValidators([Validators.required]);
+    } else {
+      this.isOnlinePaymentSelected = false;
+
+      this.checkoutForm.get('cardNumber')?.clearValidators();
+      this.checkoutForm.get('cardHolderName')?.clearValidators();
+      this.checkoutForm.get('expiryMonth')?.clearValidators();
+      this.checkoutForm.get('expiryYear')?.clearValidators();
+      this.checkoutForm.get('cvv')?.clearValidators();
+    }
+
+    this.checkoutForm.get('cardNumber')?.updateValueAndValidity();
+    this.checkoutForm.get('cardHolderName')?.updateValueAndValidity();
+    this.checkoutForm.get('expiryMonth')?.updateValueAndValidity();
+    this.checkoutForm.get('expiryYear')?.updateValueAndValidity();
+    this.checkoutForm.get('cvv')?.updateValueAndValidity();
+  }
+
+  onSubmit() {
+    if (!this.selectedPaymentMethod) {
+      this.paymentMethodError = true; 
+      return;
+    }
+
+    if (this.checkoutForm.valid) {
+      localStorage.setItem('checkoutData', JSON.stringify(this.checkoutForm.value));
+      this.router.navigate(['/order']);
+    } else {
+      Object.keys(this.checkoutForm.controls).forEach(field => {
+        const control = this.checkoutForm.get(field);
+        control?.markAsTouched({ onlySelf: true });
+      });
+      console.log('Form is invalid');
+    }
+  }
 }
